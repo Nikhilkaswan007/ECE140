2ab400b73935305ec6511de17559aa7cb25ec9cb
Nikhil Kaswan <nikhilkaswan90409@gmail.com>
2025-08-04 12:10:07 +0530

    class code

---
+++ ECE140/Python intro/day2.py
@@ -0,0 +1,45 @@
+# day2.py
+# This program calculates the average of three numbers provided by the user.
+"""
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+num3= int(input("Enter third number: "))
+average = (num1 + num2 + num3) / 3
+print("The average of the three numbers is:", average)
+"""
+# This program checks if a word is a keyword in Python and lists all keywords.
+"""
+import keyword  as kw
+b= kw.iskeyword("lambda")
+print("Is 'lambda' a keyword in Python?", b)
+b= kw.iskeyword("hello")
+print("Is 'hello' a keyword in Python?", b)
+b= kw.kwlist
+print("List of Python keywords:", b)
+"""
+# This program demonstrates the use of the 'keyword' module in Python.
+# PEMDAS () ** * / + -
+#() is for grouping 
+# ** is for exponentiation
+# * is for multiplication
+# / is for division
+# + is for addition
+# // is for integer division
+# - is for subtraction
+# print((5 + 3))
+# print ((5 + 3) ** 2)
+# print((5 + 3) ** 2 - 6 + 3)
+# This program demonstrates the use of the 'math' module in Python.
+
+print(1234%100)
+# This program calculates the remainder of 1234 divided by 100.
+# by using this we git last two digits of a number.
+
+# chick if a numner is divisible by 5 or not  user input
+num = int(input("Enter a number: "))
+if num % 5 == 0:    
+    print("The number is divisible by 5.")
+else:
+    print("The number is not divisible by 5.")
+# This program checks if a number is divisible by 5.
+
